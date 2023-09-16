6961005c1fea677b0aca1ecad16ed311bbe3cf9f
Seth Mazer <116219473+SethMazer@users.noreply.github.com>
2023-09-15 21:31:19 -0400

    Add files via upload

--- ClientSideGui.py
+++ ClientSideGui.py
@@ -1,14 +1,18 @@
+#Importing necessary libraries
 from tkinter import *
 import socket
-import time
+import struct
 import os
 
 
 #########################################
 #Setting up client server and returned data variable
 recievedData = None
+currentPage = None
 
-def startConnection():
+#Starting intitial connection with server
+def startInitialConnection():
+    global clientSocket
 
     #Setting up IPV4 and TCP protocol
     clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -17,70 +21,148 @@ def startConnection():
     serverAddress = ("72.66.11.84", 8080)
     clientSocket.connect(serverAddress)
    
-    #Sending test message to server
-    message = "Hello, desktop computer!"
-    clientSocket.send(message.encode('utf-8'));
-
     #Recieving data(message) from server
     global recievedData
     
     recievedData = clientSocket.recv(1024)
     recievedData = recievedData.decode('utf-8')
     
-    
-    #Closing process
-    clientSocket.close()
 
-    #Printing successful connection message:
-    connectionMessage = f"Sucessfully connected to server @ {'192.168.1.181:8080'}"
-    connectionMessageOut = Label(Window, text = connectionMessage, font = 10)
-    clientCanvas.create_window(300,425, window = connectionMessageOut)
+    #Closing process
+    #clientSocket.close()
 
     #Printing data out to GUI
     dataMessage = recievedData
-    dataMessageOut = Label(Window, text = dataMessage, font = 15)
-    clientCanvas.create_window(300, 500, window = dataMessageOut)
+    dataMessageOut = Label(Window, text = dataMessage, font = 15, bg = "cadetblue1")
+    homePage.create_window(300, 400, window = dataMessageOut)
 
-   
+    createContinueButton()
 
 
+#Getting data from server by sending row number to servers row variable, to then traverse row data and return the string at specified row
+#Then recieving data back from server and displaying data at specified row
+def getData():
+    
+    #getting row number, packing it to struct, turn int to bytes to send over
+    dataRowLine = int(dataRow.get())
+    dataForTransit = struct.pack("!I", dataRowLine)
+
+    #Sending row num to server
+    clientSocket.send(dataForTransit)
+
+    #Recieving data back and printing it to GUI
+    dataRecieved = clientSocket.recv(1024)
+    dataRecieved = dataRecieved.decode('utf-8')
+
+    print(dataRecieved)
+
+    dataToDisplayFont = ("Helvetica", 12)
+    dataToDisplay = dataRecieved
+    dataToDisplayOut = Label(Window, text = dataToDisplay, font = dataToDisplayFont, bg = "cadetblue1")
+    dataPage.create_window(300, 400, window = dataToDisplayOut)
+
+    clientSocket.close()
+
+
+
+
+
+
+#Logging client actions, if client clicks continue, send server message that client moved to data viewing else do sum else
+def clientActions(clientActionCode):
+    #Moved Page
+    if(clientActionCode == 1):
+        actionMessage = "Client moved to data viewing page"
+        clientSocket.send(actionMessage.encode('utf-8'))
+    elif(clientActionCode == 2):
+        actionMessage = "Client did sum else ????"
+        clientSocket.send(actionMessage.encode('utf-8'))
+
+
+
+#Function for creating continueButton
+def createContinueButton():
+
+    #Commands for continueButton
+    def continueButtonCommands():
+        clientActions(1)
+        changePage()
+
+    #Constructing continue button
+    continueButton = Button(Window, text = "Continue", height = 3, width = 8, command=continueButtonCommands)
+    homePage.create_window(300, 450, window = continueButton)
+
+    
 
 ##############################################
-#Setting up tkinter gui
-Window = Tk()
+#Home page setup
+def goHomePage():
+    #Setting up Canvas
+    global homePage
+    homePage = Canvas(Window, width = 600, height = 600, bg = "cadetblue1")
+    homePage.pack()
 
-#Window Geometry
+    #Decorating canvas, setting up buttons, setting up lables and such.
+    #Consturcting welcome message
+    getClientUserName = os.path.expanduser("~");
+    clientUserName = os.path.basename(getClientUserName)
+
+    #Welcome message/label
+    welcomeFont = ("Helvetica", 25)
+    welcomeMessage = f"Welcome {clientUserName}"
+    welcomeLabel = Label(Window, text = welcomeMessage, font=welcomeFont, bg="cadetblue1")
+    homePage.create_window(300,150, window = welcomeLabel)
+
+
+    #Constructing connect button
+    connectButton = Button(Window, text="Connect",font = 15  ,border = 5, command = startInitialConnection, height = 5, width = 20,)
+    homePage.create_window(300,300, window = connectButton)
+
+
+#Data page setup
+def goDataPage():
+    #Setting up canvas
+    global dataPage
+    dataPage = Canvas(Window, width = 600, height = 600, bg = "cadetblue1")
+    dataPage.pack()
+
+    #Get random car brand label
+    getDataLabelFont = ("Helvetica", 25)
+    getDataLabel = Label(Window, text = "Click button to get data from server", font = getDataLabelFont, bg = "cadetblue1")
+    dataPage.create_window(300,150, window = getDataLabel)
+
+
+    #Enter Row Label
+    enterRowFont = ("Helvetica", 12)
+    enterRowLabel = Label(Window, text = "Enter row for data field", font = enterRowFont, bg = "cadetblue1")
+    dataPage.create_window(300, 200, window = enterRowLabel)
+
+    #Enter row data entry field
+    global dataRow
+    dataRow = Entry(dataPage)
+    dataPage.create_window(300, 250, window = dataRow)
+
+
+    #Get data button
+    getDataButton = Button(Window, text = "get Data", border = 5, height = 3, width = 8, command = getData)
+    dataPage.create_window(300,300 , window = getDataButton)
+
+
+
+
+#Changing pages, by hiding current widgets and showing next
+def changePage():
+    homePage.pack_forget()
+    goDataPage()
+
+
+#Setting up tkinter window a geometry
+Window = Tk()
 Window.geometry("600x600")
 Window.title("Client Portal")
 
-#Setting up Canvas
-clientCanvas = Canvas(Window, width = 600, height = 600)
-clientCanvas.pack()
-
-
-#Decorating canvas, setting up buttons, setting up lables and such.
-
-#Consturcting welcome message
-getClientUserName = os.path.expanduser("~");
-clientUserName = os.path.basename(getClientUserName)
-
-welcomeMessage = f"Welcome {clientUserName}"
-
-welcomeLabel = Label(Window, text = welcomeMessage, font=("Arial", 25))
-clientCanvas.create_window(300,150, window = welcomeLabel)
-
-
-
-#Constructing to connect button
-connectButton = Button(Window, text="Connect", font = 25, border = 5, command = startConnection, height = 5, width = 20)
-clientCanvas.create_window(300,300, window = connectButton)
-
-
-
-
-
-
-
+#Showing page
+goHomePage()
 
 
 
--- ServerSide.py
+++ ServerSide.py
@@ -1,37 +1,85 @@
 #Importing library
 import socket
+import struct
 import time
 
-#Setting up socket type (ipv4) and socket protocal (tcp)
+#Setting up socket type (ipv4) and socket protocol(tcp)
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #Setting up host and port
-host = "192.168.1.162"
+host = "192.168.1.181"
 port = 8080
 
-#Binding endpoint to this server computer, and printing listening statement
+#Binding endpoint to this server computer and printing listening statement
 server_socket.bind((host, port))
 print(f"Server is listening on {host}:{port}")
 
-#Listening for incoming connections using .listen
+#Listening for inoming connections using .listen
 server_socket.listen(5)
 
 #Accept incoming connections and printing acceptance statement
-client_socket, client_adress = server_socket.accept()
-print(f"Accepted connection from {client_adress}")
+clientSocket, client_address = server_socket.accept()
+print(f"Accepted Connection from {client_address}")
 
-#Recieve data from client computer, and printing recieved statement
-data = client_socket.recv(1024);
-print(f"Received data: {data.decode('utf-8')}")
+#Recieve data from client and print recieved statement
+# data = clientSocket.recv(1024)
+# print(f"Recieved data: {data.decode('utf-8')}")
 
 
-#Sending response back to client computer
-response = "Hello, connected to blue laptop!"
-client_socket.send(response.encode('utf-8'));
+#Sending response back to client
+response = "Sucessfully connected to server"
+clientSocket.send(response.encode('utf-8'))
 
-#Closing both client and server sockets
+#Recieve data from client and print recieved statement
+clientAction = clientSocket.recv(1024)
+print(f"Client Action: {clientAction.decode('utf-8')}")
 
-client_socket.close()
+##############################################
+#Accessing data
+
+dataRowBytes = (clientSocket.recv(4))
+dataRowInt = (struct.unpack("!I", dataRowBytes)[0])
+
+print(f"Data row requested: {dataRowInt}")
+
+#Traversing carData.txt
+
+#Current row of file
+currentRow = 0
+
+data = None
+with open(r"C:\Users\boomc\OneDrive\Documents\Server-Side\carData.txt", "r") as file:
+    
+    #Looping through each row in carData.txt
+    for line in file:
+        currentRow += 1
+        if currentRow == dataRowInt:
+            data = line.strip()
+            print(data)
+            
+            
+#Sending data back to client
+dataForTransit = data.encode('utf-8')
+clientSocket.send(dataForTransit)
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Closing client and server sockets
+time.sleep(100)
+
+clientSocket.close()
 server_socket.close()
 
 time.sleep(75)
